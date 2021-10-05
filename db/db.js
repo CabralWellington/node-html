1d5b290391a63a6337e11128f01afe2918221204
Wellington Oliveira <wellington_oliveira@live.com>
2021-10-05 17:50:19 -0400

    first commit

--- db/db.js
+++ db/db.js
@@ -2,21 +2,19 @@ const mysql = require("mysql2/promise");
 
 var config =
 {
-    host: '192.168.0.30',
-    user: 'Admin',
-    password: 'Admin@123',
+    host: '192.168.0.29',
+    user: 'root',
+    password: 'strongpassword',
     database: '_mysql',
-    port: 3306,
-    ssl: false
 };
 
 async function connect(){
     if(global.connection && global.connection.state !== 'disconnected'){
-        console.log("MySql Connection Recovered");
+        //console.log("MySql Connection Recovered");
         return global.connection;
     }
     const connection = await mysql.createConnection(config);
-    console.log("MySql Connection Started");
+    //console.log("MySql Connection Started");
     global.connection = connection;
     return connection;
 }
